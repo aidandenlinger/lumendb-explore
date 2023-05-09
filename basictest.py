341c593d1a5c37e40d6305f8df84e7eeee7060dd
Aidan Denlinger <47428697+aidandenlinger@users.noreply.github.com>
2023-05-09 15:09:12 -0700

    style(basictest): make it print neater :)

--- basictest.py
+++ basictest.py
@@ -1,5 +1,7 @@
+import json
 import logging
 from os import getenv
+from typing import Any
 
 from dotenv import load_dotenv
 
@@ -13,7 +15,13 @@ if not api_key:
     print("A Lumen API key needs to be in a .env file, please see the README")
     exit(1)
 
+
+def pprint(data: dict[str, Any]):
+    """Pretty print JSON."""
+    print(json.dumps(data, indent=2))
+
+
 with LumenAPIManager(api_key) as api:
     print(api.get_topics())
-    print(api.search_entity("Youtube Inc", per_page=50))
-    print(api.get_notice(5))
+    pprint(api.search_entity("Youtube Inc", per_page=5))
+    pprint(api.get_notice(5))
